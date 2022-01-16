b7722b23c5394937cedcfb7f3e58156b77fb7d55
Polski Arrow <polskiarrow@protonmail.com>
2022-01-16 16:18:49 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_CLion_Git_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_CLion_Git_3.dir/main.cpp.obj"
+  "OU_CLion_Git_3.exe"
+  "OU_CLion_Git_3.exe.manifest"
+  "OU_CLion_Git_3.pdb"
+  "libOU_CLion_Git_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU_CLion_Git_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OU_CLion_Git_3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OU_CLion_Git_3.
+# This may be replaced when dependencies are built.
